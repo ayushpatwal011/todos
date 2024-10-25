e45b0cb50bccca09dff2996273589609127c69c4
ayushpatwal011 <patwalayush2005@gmail.com>
2024-10-25 21:37:30 +0530

    first commit

--- index.js
+++ index.js
@@ -18,12 +18,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: "GET, POST, PUT, DELETE",
-    allowedHeaders: ["Content-Type", "Authorization"]
-  })
+  cors()
 )
 
 // Database connection
